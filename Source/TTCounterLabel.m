d0d49320efb8dd1dc8b9ec1f259ebb0764ee3637
PeiSong Xiong <peisong@outlook.com>
2014-02-04 10:27:41 +0800

    Added display mode for seconds only display

--- Source/TTCounterLabel.m
+++ Source/TTCounterLabel.m
@@ -94,7 +94,7 @@
     // ensure we see the last digit go to zero.
     if (self.countDirection == kCountDirectionDown && _value < 50 && self.isRunning) {
         [self stop];
-        self.valueString = @"00s.00";
+        self.valueString = self.displayMode == kDisplayModeFull ? @"00s.00" : @"00s";
         
         // Inform any delegates
         if (self.countdownDelegate && [self.countdownDelegate respondsToSelector:@selector(countdownDidEnd)]) {
@@ -190,12 +190,12 @@
     
     if (hrs == 0) {
         if (mins == 0) {
-            formattedString = [NSString stringWithFormat:@"%02llus.%02llu", secs, frac];
+            formattedString = self.displayMode == kDisplayModeFull ? [NSString stringWithFormat:@"%02llus.%02llu", secs, frac] : [NSString stringWithFormat:@"%02llus", secs];
         } else {
-            formattedString = [NSString stringWithFormat:@"%02llum %02llus.%02llu", mins, secs, frac];
+            formattedString = self.displayMode == kDisplayModeFull ? [NSString stringWithFormat:@"%02llum %02llus.%02llu", mins, secs, frac] : [NSString stringWithFormat:@"%02llum %02llus", mins, secs];
         }
     } else {
-        formattedString = [NSString stringWithFormat:@"%02lluh %02llum %02llus.%02llu", hrs, mins, secs, frac];
+        formattedString = self.displayMode == kDisplayModeFull ? [NSString stringWithFormat:@"%02lluh %02llum %02llus.%02llu", hrs, mins, secs, frac] : [NSString stringWithFormat:@"%02lluh %02llum %02llus", hrs, mins, secs];
     }
     
     return formattedString;
